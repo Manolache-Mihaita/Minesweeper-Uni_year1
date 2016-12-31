e520ad74ad984a4f0394098cc3c2e8b5905631aa
manol <manol@DESKTOP-8564KLL>
2016-12-31 21:41:57 +0200

    to do: if enter on blank spot if click on bomb bomb count ( if good end level) maby scoreboard next version ability to draw a random level? work with sfml? maby?

--- Minesweeper-Uni_year1/Minesweeper-Uni_year1.cpp
+++ Minesweeper-Uni_year1/Minesweeper-Uni_year1.cpp
@@ -1,18 +1,34 @@
 ﻿// Minesweeper-Uni_year1.cpp : Defines the entry point for the application.
 //
-
 #include "stdafx.h"
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
 #include <string.h>
 #include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
 #define m_col 300
 #define m_line 300
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define KEY_ENTER 13
+#define pos char(219)
+#define gen_top char(205)
+#define gen_side char(186)
+#define gen_top_left char(201)
+#define gen_top_right char(187)
+#define gen_bot_left char(200)
+#define gen_bot_right char(188)
+#define c_bomb char(167)
 using namespace std;
 
-
-
+signed char mat_cout[300][300];
+signed char mat_back[300][300];
+int lin;
+int col;
 /*#define MAX_LOADSTRING 100
 
 // Global Variables:
@@ -241,39 +257,192 @@ void gen_numbers(signed char mat[][300], int lin, int col) {
 				if (mat[i + 1][j - 1] == 'B')count++;
 				if (mat[i + 1][j] == 'B')count++;
 				if (mat[i + 1][j + 1] == 'B')count++;
-				mat[i][j] = count + '0';
+				if (count == 0)mat[i][j] = char(0);
+				else mat[i][j] = count + '0';
 			}
 		}
 }
-int main() {
-	signed char a[300][300];
-	int lin;
-	int col;
-	cin >> lin;
-	cin >> col;
+void gen_start(signed char mat[][300], int lin, int col) {
 	for (int i = 0; i < lin; i++)
 		for (int j = 0; j < col; j++) {
-			a[i][j] = char(35);
+			mat[i][j] = char(35);
 		}
-	int Bombs;
-	cin >> Bombs;/*
-	int count = 0;
-	while (count < Bombs) {
-		long  q = (rand() + time(0)) % lin;
-		long  b = (rand() + time(0)) % col;
-		a[q][b] = 0;
-		count++;
-	}*/
-	gen_border(a, lin, col);
-	gen_bombs(a, Bombs, lin, col);
-	gen_numbers(a, lin, col);
+}
+void gen_start_cout(signed char mat[][300], int lin, int col) {
+	for (int i = 0; i < lin; i++)
+		for (int j = 0; j < col; j++) {
+			mat[i][j] = char(233);
+		}
+}
+void draw(signed char mat[][300],int lin,int col) {
 	for (int i = 0; i < lin; i++) {
 		for (int j = 0; j < col; j++) {
-			cout << a[i][j];
+			cout << mat[i][j];
+		}
+		cout << '\n';
+	}
+	return;
+}
+//void if_pos_move(signed char mat_back[][300], signed char mat_cout[][300], int lin, int col, int i_c, int j_c)
+
+// add to every function that calls this  lin and call or be a smartass and make em global
+void uncover_area(signed char mat_back[][300], signed char mat_cout[][300], int i_c, int j_c) {
+}
+void if_pos_move(signed char mat_back[][300],signed char mat_cout[][300], int i_c, int j_c, unsigned char &o) {
+	//if (mat_back[i_c][j_c] == 'B' || mat_back[i_c][j_c] == char(0))
+	if (mat_back[i_c][j_c] <= '9'&& mat_back[i_c][j_c] >= '0') {
+		mat_cout[i_c][j_c] = mat_back[i_c][j_c];
+		o = mat_back[i_c][j_c];
+	}
+	if (mat_back[i_c][j_c] == char(0)) {
+		uncover_area(mat_back, mat_cout, i_c, j_c);
+		o = char(0);
+
+	}
+}
+void move(signed char mat_cout[][300], int lin, int col, signed char mat_back[][300]) {
+	unsigned char o;
+	o = mat_cout[0][0];
+	int i_c = 0;
+	int j_c = 0;
+	int c;
+	mat_cout[0][0] = pos;
+	while (1)
+	{
+		draw(mat_cout, lin, col);
+		//Sleep(1000);
+		switch ((c = _getch())) {
+		case KEY_UP:
+			mat_cout[i_c][j_c] = o;
+			i_c--;
+			if (i_c < 0)i_c = lin-1;
+			i_c %= lin;
+			o = mat_cout[i_c][j_c];
+			mat_cout[i_c][j_c] = pos;
+			system("CLS");
+			//cout << endl << "Up" << endl;//key up
+			//system("CLS");
+			break;
+		case KEY_DOWN:
+			mat_cout[i_c][j_c] = o;
+			i_c++;
+			i_c %= lin;
+			o = mat_cout[i_c][j_c];
+			mat_cout[i_c][j_c] = pos;
+			system("CLS");
+			//cout << endl << "Down" << endl;   // key down
+			//system("CLS");
+			break;
+		case KEY_LEFT:
+			mat_cout[i_c][j_c] = o;
+			j_c--;
+			if (j_c < 0)j_c = col-1;
+			j_c %= col;
+			o = mat_cout[i_c][j_c];
+			mat_cout[i_c][j_c] = pos;
+			system("CLS");
+			//cout << endl << "Left" << endl;  // key left
+			//system("CLS");
+			break;
+		case KEY_RIGHT:
+			mat_cout[i_c][j_c] = o;
+			j_c++;
+			j_c %= col;
+			o = mat_cout[i_c][j_c];
+			mat_cout[i_c][j_c] = pos;
+			system("CLS");
+			//cout << endl << "Right" << endl;  // key right
+			//system("CLS");
+			break;
+			//default:
+			//cout << endl << "null" << endl;  // not arrow
+			//break;
+		case KEY_ENTER:
+			o = mat_cout[i_c][j_c];
+			if_pos_move(mat_back, mat_cout, i_c, j_c, o);
+			mat_cout[i_c][j_c] = o;
+			system("CLS");
+			break;
+		default:cout << c;
+		}
+
+	}
+}
+void start() {
+	signed char mat_back[300][300];
+	signed int lin = 22;
+	signed int col = 52;
+	signed int Bombs = 80;
+	gen_start(mat_back, lin, col);
+	gen_border(mat_back, lin, col);
+	gen_bombs(mat_back, Bombs, lin, col);
+	gen_numbers(mat_back, lin, col);
+	// cout for mat_back -- matrix with the numbers+bombs
+	/*for (int i = 0; i < lin; i++) {
+		for (int j = 0; j < col; j++) {
+			cout << mat_back[i][j];
+		}
+		cout << '\n';
+	}*/
+	signed char mat_cout[300][300];
+	gen_start_cout(mat_cout, lin, col);
+	gen_border(mat_cout, lin, col);
+	/*for (int i = 0; i < lin; i++) {
+		for (int j = 0; j < col; j++) {
+			cout << mat_cout[i][j];
 		}
 		cout << '\n';
 	}
 
+	//draw(mat_cout, lin, col);
+
+	// display things? i hope so?*/
+	move(mat_cout, lin, col, mat_back);
+
+
+
+	}
+
+
+
+
+
+
+int main() {
+
+	start();
+	/*int a = 1;
+	while (a)
+		if (GetKeyState('S') < 0) {
+			cout << 100;
+			break;
+		}*/
+	/*int c ;
+	while (1)
+	{
+		Sleep(100);
+		c;
+		switch ((c = _getch())) {
+		case KEY_UP:
+			cout << endl << "Up" << endl;//key up
+			break;
+		case KEY_DOWN:
+			cout << endl << "Down" << endl;   // key down
+			break;
+		case KEY_LEFT:
+			cout << endl << "Left" << endl;  // key left
+			break;
+		case KEY_RIGHT:
+			cout << endl << "Right" << endl;  // key right
+			break;
+		//default:
+			//cout << endl << "null" << endl;  // not arrow
+			//break;
+		}
+
+	}*/
 	return 0;
+	//use to clear console
+	//system("CLS");
 
 }
